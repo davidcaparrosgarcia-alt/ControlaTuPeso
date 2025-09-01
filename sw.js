bf05feae2ed0618be365d0021d62a80353092b95
davidcaparrosgarcia-alt <davidcaparrosgarcia@gmail.com>
2025-09-01 21:02:58 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-// sw.js - v2.0 con Estrategia Stale-While-Revalidate
+// sw.js - v2.1 con Estrategia Stale-While-Revalidate
 
-const CACHE_VERSION = 'v2.0.1-beta'; // Versión para el gran cambio
+const CACHE_VERSION = 'v2.1.1-beta'; // Versión para el gran cambio
 const CACHE_NAME = `controlatupeso-cache-${CACHE_VERSION}`;
 
 const urlsToCache = [
